82268194472f8b5a837e4b0eeee452672fec7fa0
alexisrougnant <alexis.rougnant@gmail.com>
2016-11-07 18:10:44 +0100

    unity prototype

---
+++ Unity/AfterTheLastTrainIsGone/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+	public GameObject textBox;
+	public TextAsset textFile;
+
+	public Text textToDisplay;
+
+	public string[] textLines;
+
+	public int currentLine;
+
+	public int endLine;
+
+	[Range (0,10)]
+	public int numberOfLinesToShow;
+
+
+	// Use this for initialization
+	void Start () {
+		if (textFile != null) {
+			textLines = textFile.text.Split ('\n');
+		}
+
+		if (endLine == 0) {
+			endLine = textLines.Length - 1;
+		}
+
+		Next ();
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			Next ();
+		}
+	}
+
+	void Next(){
+		textToDisplay.text = "";
+		if (currentLine <= endLine) {
+			textToDisplay.text = textLines [currentLine];
+			for (int i = 1; i < numberOfLinesToShow; i++) {
+				textToDisplay.text += System.Environment.NewLine + textLines [currentLine + i];
+			}
+			currentLine += numberOfLinesToShow;
+			numberOfLinesToShow = Mathf.Min (numberOfLinesToShow, endLine-currentLine);
+
+		}
+		
+	}
+}
