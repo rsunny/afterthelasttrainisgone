5777b3c9a21d2512081749e149003649d91d40da
fillippe <jovanealberto@hotmail.com>
2017-02-05 02:17:47 +0100

    Finished Final level and bug fixing

---
+++ Unity/Assets/Scripts/EndScreen/EndScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndScript : MonoBehaviour {
+
+	public string buttonToExit = "Fire3";
+	public string sceneToLoad = "Menu";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire3"))
+		{
+			SceneManager.LoadScene (sceneToLoad);
+		}
+		
+	}
+}
---
+++ Unity/Assets/Scripts/Interaction Script/New Interaction Script/CreateDestroyObjectsAtState.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDestroyObjectsAtState : MonoBehaviour {
+
+	public int fromState = 0;
+	private int curState = 0;
+	private bool done = false;
+	public GameObject objectToCreate = null;
+	public GameObject objectToDestroy = null;
+
+	void OnTriggerExit (Collider collideObj)
+	{
+		if (collideObj.tag == "Player" && !done)
+		{
+			curState = StateManager.currentState;
+			if(curState >= fromState)
+			{
+				if (objectToDestroy != null)
+				{
+					objectToDestroy.SetActive(false);
+				}
+				if (objectToCreate != null)
+				{
+					objectToCreate.SetActive(true);
+				}
+				done = true;
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
